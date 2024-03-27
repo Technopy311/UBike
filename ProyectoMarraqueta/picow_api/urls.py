bdf81c18ae020c6d5bb9038fbad78eca68625e59
Technopy311 <xmenmagnet@gmail.com>
2024-03-27 08:15:23 -0300

    (picow_api) create skeleton urls and views

--- ProyectoMarraqueta/picow_api/urls.py
+++ ProyectoMarraqueta/picow_api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views as picow_api_views
 
 urlpatterns = [
-
+    path("recv", picow_api_views.recv, name="picow_recv"),
+    path("auth_user", picow_api_views.auth_user, name="picow_auth"),
+    path("controller", picow_api_views.controller, name="picow_controller")
 ]
--- ProyectoMarraqueta/picow_api/views.py
+++ ProyectoMarraqueta/picow_api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def recv():
+    pass
+
+
+def auth_user():
+    pass
+
+
+def controller():
+    pass
+
