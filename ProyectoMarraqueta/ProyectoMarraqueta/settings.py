d512fef7d89eeb32e101996050dd3dcf1076bec8
Technopy311 <xmenmagnet@gmail.com>
2024-04-10 21:40:32 -0400

    chore(settings.py): Setup crispyforms plugins

--- ProyectoMarraqueta/ProyectoMarraqueta/settings.py
+++ ProyectoMarraqueta/ProyectoMarraqueta/settings.py
@@ -38,8 +38,14 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 	"core",
-	"picow_api"
+	"picow_api",
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
