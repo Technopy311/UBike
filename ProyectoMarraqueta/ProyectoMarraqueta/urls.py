5eff499a064c438f935793471e0e3e961225d464
Technopy311 <xmenmagnet@gmail.com>
2024-04-10 21:39:57 -0400

    feat(core urls): Add django.contrib.auth urls

--- ProyectoMarraqueta/ProyectoMarraqueta/urls.py
+++ ProyectoMarraqueta/ProyectoMarraqueta/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api", include("picow_api.urls"), name="picow_api"),
     path("", include("core.urls"), name="core"),
+    path("auth", include("django.contrib.auth.urls")),
 ]
