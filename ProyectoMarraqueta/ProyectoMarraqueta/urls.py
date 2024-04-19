0ba3e7abd7293b9d77e705b4fe2b81ffdb95c410
Technopy311 <xmenmagnet@gmail.com>
2024-04-19 08:05:01 -0400

    chore(ProyectoMarraqueta.urls): reorder urls.

--- ProyectoMarraqueta/ProyectoMarraqueta/urls.py
+++ ProyectoMarraqueta/ProyectoMarraqueta/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api", include("picow_api.urls"), name="picow_api"),
     path("", include("core.urls"), name="core"),
-    path("auth", include("django.contrib.auth.urls")),
+    path("api/", include("picow_api.urls"), name="picow_api"),
+    path("auth/", include("django.contrib.auth.urls")),
+    path("admin/", admin.site.urls),
 ]
