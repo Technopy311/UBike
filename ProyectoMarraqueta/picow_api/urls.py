e520512e95de757f26ce7d47d2d846feaffb75d3
Technopy311 <xmenmagnet@gmail.com>
2024-04-24 18:09:19 -0400

    chore(picow_api): Add comments to views.py and urls.py. Add nice title to the picow communication draft

--- ProyectoMarraqueta/picow_api/urls.py
+++ ProyectoMarraqueta/picow_api/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
 from . import views as picow_api_views
 
+""" 
+
+Endpoints:
+
+Endopoint: recv.
+
+Description: 
+
+This endpoint is the one which receives the connection from the pico_w module.
+There are two other packages underlying this endpoint, "controller" and "auth_user"
+"""
+
 urlpatterns = [
-    path("recv", picow_api_views.recv, name="picow_recv"),
-    path("auth_user", picow_api_views.auth_user, name="picow_auth"),
-    path("controller", picow_api_views.controller, name="picow_controller")
+    path("recv", picow_api_views.recv, name="picow_recv")
 ]
--- ProyectoMarraqueta/picow_api/views.py
+++ ProyectoMarraqueta/picow_api/views.py
@@ -1,14 +1,35 @@
 from django.shortcuts import render, redirect
-
-
-def recv():
-    pass
+from django.http import HttpResponse
 
 
 def auth_user():
     pass
 
 
-def controller():
+def controller(keychain_uuid, picow_ip_addr):
+    # Receive keychain_uuid
+    # Find the Bicycleholder instance corresponding to picow_ip_addr
+    # Get the KeyChain object related to kychain_uuid
+    # Get the user related to KeyChain instance.
+    # Create a list which contains the user's bicycle's PKs
+    # Check which of the PKs is in the Bicycleholder
+    
     pass
 
+
+def recv(request):
+    if request.method == "POST":
+        # Receive UUID
+        # Parse keychain UUID
+        # Get the pico_w module's ip address.
+        
+        # Pass UUID and pico_w's ip address, 
+        # to controller function.
+        
+        pass
+    else:
+        response = HttpResponse()
+        response.status_code = 400
+        response.closed = True
+
+        return response
