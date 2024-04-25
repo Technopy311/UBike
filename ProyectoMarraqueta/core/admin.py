0b506991b4005b3fb20e6a43d3789135096d4810
Technopy311 <xmenmagnet@gmail.com>
2024-04-24 21:11:22 -0400

    feat(core.models & core.admin): Set up CustomUserAdmin and register core.models's models in admin

--- ProyectoMarraqueta/core/admin.py
+++ ProyectoMarraqueta/core/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models as core_models
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        *UserAdmin.fieldsets,
+        (
+            'New Fields',
+            {
+                'fields': (
+                    'is_guard',
+                    'base_role',
+                    'name',
+                    'user_type',
+                    'last_name',
+                    'run',
+                ),
+            },
+        ),
+    )
+
+admin.site.register(core_models.User, CustomUserAdmin)
+admin.site.register(core_models.Student, CustomUserAdmin)
+admin.site.register(core_models.Professor, CustomUserAdmin)
+admin.site.register(core_models.Academic, CustomUserAdmin)
+admin.site.register(core_models.External, CustomUserAdmin)
+admin.site.register(core_models.Staff, CustomUserAdmin)
+admin.site.register(core_models.Guard, CustomUserAdmin)
+
+#admin.site.register(core_models.Guard)
