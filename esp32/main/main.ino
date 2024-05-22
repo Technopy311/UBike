97401bf2b506e767fb581bc0c201c54bff6a850e
Technopy311 <xmenmagnet@gmail.com>
2024-05-21 20:20:10 -0400

    chore/perf(esp32.main): Cleanup the code and optimize a little of memory Usage

--- esp32/main/main.ino
+++ esp32/main/main.ino
@@ -1,6 +1,4 @@
 //Include necessary dependencies/libraries
-
-
 #include <WiFi.h>
 #include <assert.h>
 #include <Arduino.h>
@@ -13,7 +11,8 @@
 #define RST_PIN  0
 #define SS_PIN 21 // GPIO 21 ESP32
 
-String server_url = "http://192.168.99.193:8080/api/recv";
+
+const char* server_url = "http://192.168.99.193:8080/api/recv";
 
 // Create mfrc522 instance of MFRC522 Class
 MFRC522 mfrc522(SS_PIN, RST_PIN);
@@ -25,7 +24,9 @@ byte nuidPICC[4]; // Init array that will store new NUID
 /*
   Function that sends a post request to the given url
 */
-bool post_uuid(String url, String uuid, String ipaddr){
+bool status post_uuid(String url, String uuid, String ipaddr){
+  // Returns True if good response; else False.
+  
   String ip; // Create ip String
   HTTPClient http; // Create instance of HTTPClient
   JSONVar dataObject; // Create json instance of JSONVar
@@ -33,19 +34,16 @@ bool post_uuid(String url, String uuid, String ipaddr){
   Serial.print("[HTTP] begin...\n");
   http.begin(url);
 
-  dataObject["uuid"] = String(uuid); // Add the uid data to the json object
+  dataObject["uuid"] = char(uuid); // Add the uid data to the json object
   dataObject["ip"] = ipaddr; // Add the ip address to the json objet
 
   /*
   The json object currently looks as follows:
-
   {
     "uuid": uuid,
     "ip": ipaddr
   }
-  
   */
-
   String jsonString = JSON.stringify(dataObject); // Cast the json object into String
 
   Serial.print("[HTTP] POST... \n");
@@ -55,11 +53,13 @@ bool post_uuid(String url, String uuid, String ipaddr){
   http.addHeader("Content-Type", "application/json"); // Add the application/json HTTP header to the http object
   int httpResponseCode = http.POST(jsonString); // Execute the POST request and save the HTTP response code into httpResponseCode int variable
   
+    
+
+
   Serial.print(httpResponseCode); // Print the response code
   Serial.print("\n");
   http.end();
 }
-
 
 String getID(){
   if (! mfrc522.PICC_ReadCardSerial()){
@@ -71,44 +71,38 @@ String getID(){
   hex_num += mfrc522.uid.uidByte[2] << 8;
   hex_num += mfrc522.uid.uidByte[3];
   mfrc522.PICC_HaltA(); // Stop reading
-  return String(hex_num);
+  return char(hex_num);
 }
 
 
 
 void setup() {
   Serial.begin(115200); // Begin serial monitor connection
-  Serial.print("\n\n\n");
-
   SPI.begin();
+  
   mfrc522.PCD_Init(); // Init mfrc522 rfid reader card
 
-  delay(5);
   //mfrc522.PCD_DumpVersionToSerial();	// Show details of PCD - MFRC522 Card Reader details
 
-  for(uint8_t t=4; t>0; t--){ // Wait until full boot
-    Serial.printf("[SETUP] WAIT %d...\n", t);
-    Serial.flush();
-    delay(500);
-  }
-  WiFi.mode(WIFI_STA);
+  delay(2000)
   WiFi.begin("equisde", "lol1234xd"); // Connect to WiFi Access Point
+  while(WiFi.status() != WL_CONNECTED){
+    delay(250);
+    Serial.print(".");
+  }
+  Serial.print("\n")
 }
 
 
 void loop() {
-  Serial.print("Reading C:\n");
+  Serial.print("Reading\n");
   while(true){
-    //Serial.printf("\nStack:%d,Heap:%lu\n", uxTaskGetStackHighWaterMark(NULL), (unsigned long)ESP.getFreeHeap());
-
     // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
-    if (mfrc522.PICC_IsNewCardPresent() && (WiFi.status() == WL_CONNECTED)){
+    if (mfrc522.PICC_IsNewCardPresent()){
 
-      String uid = getID();
-      if(uid != 0){
-        String ipaddr = WiFi.localIP().toString();
-        bool post_status = post_uuid(server_url, uid, ipaddr); // Call the post_uuid function
-      }
+      char* uid = getID();
+      char* ipaddr = WiFi.localIP().toString();
+      bool response_status = post_uuid(server_url, uid, ipaddr); // Call the post_uuid function
   
     }
     
