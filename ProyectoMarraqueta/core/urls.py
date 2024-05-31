feb32e61319784205f8cb98eda726a77e1080be7
Technopy311 <xmenmagnet@gmail.com>
2024-05-30 21:14:51 -0400

    feat(core): add index url and setup views for every url

--- ProyectoMarraqueta/core/urls.py
+++ ProyectoMarraqueta/core/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("user_view", core_views.user_view, name="user_view"),
     path("user_register", core_views.user_register, name="user_register"),
     path("guard_view", core_views.guard_view, name="guard_view"),
+    path("", core_views.welcome_view, name="welcome_view")
 ]
--- ProyectoMarraqueta/core/views.py
+++ ProyectoMarraqueta/core/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, redirect
 
 
-def user_view():
+def welcome_view(request):
+    return render(request, 'core/welcome.html')
+
+def user_view(request):
+    return render(request, 'core/welcome.html')
+
+def user_register(request): # View which receives from / to register
     pass
 
-
-def user_register():
+def login_user(request): # View to login users
     pass
 
-
-def guard_view():
-    pass
+def guard_view(request): # View of security dashboard
+    return render(request, 'core/security_dashboard.html')
