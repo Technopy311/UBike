0e51119c19e2c937f55ec8f70f3e1f66bc0b4bf6
Technopy311 <xmenmagnet@gmail.com>
2024-06-07 08:11:44 -0400

    feat!(appauth): Setup initial views and urls

--- ProyectoMarraqueta/ProyectoMarraqueta/urls.py
+++ ProyectoMarraqueta/ProyectoMarraqueta/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("core.urls"), name="core"),
-    path("api/", include("picow_api.urls"), name="picow_api"),
-    path("auth/", include("django.contrib.auth.urls")),
+    path("", include("core.urls")),
+    path("api/", include("picow_api.urls")),
     path("admin/", admin.site.urls),
+    path("auth/", include("appauth.urls"))
 ]
---
+++ ProyectoMarraqueta/appauth/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def login(request):
+    pass
+
+def register(request):
+    pass
+
+def changepass(request):
+    pass
+
+def logout(request):
+    pass
