ed2003238075c6645679f4a07c88b0974a6e7331
Technopy311 <xmenmagnet@gmail.com>
2024-06-07 08:23:59 -0400

    feat(appauth.views): Add basic login functionality

--- ProyectoMarraqueta/appauth/views.py
+++ ProyectoMarraqueta/appauth/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
+from core import models as core_models
+
+from django.core.exceptions import PermissionDenied
 
 def login(request):
-    pass
+    if request.method == "POST":
+        mail = request.POST["mail"]
+        password = request.POST["password"]
+
+        try:
+            username = core_models.User.objects.get(email=mail).username
+        except core_models.User.DoesNotExist:
+            return redirect(request, 'welcome_view') #TODO: add error message functionality for welcome_view
+        
+        try:
+            user = authenticate(username=username, password=password)
+        except PermissionDenied:
+            return redirect(request, 'welcome_view') #TODO: add error message functionality for welcome_view
+
+        return redirect(request, 'user_view')
+
+    else:
+        redirect(request, 'welcome_view')
 
 def register(request):
     pass
