36a35058ffb5c6bce2d8ef75df1dda3a46233a5a
Technopy311 <xmenmagnet@gmail.com>
2024-06-07 08:38:42 -0400

    feat!(appauth.views): Add proper login and logout functionality

--- ProyectoMarraqueta/appauth/views.py
+++ ProyectoMarraqueta/appauth/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
 from core import models as core_models
 
 from django.core.exceptions import PermissionDenied
@@ -19,6 +19,11 @@ def login(request):
         except PermissionDenied:
             return redirect(request, 'welcome_view') #TODO: add error message functionality for welcome_view
 
+        if user is not None:
+            login(request, user)
+        else:
+            return redirect(request, 'welcome_view')
+
         return redirect(request, 'user_view')
 
     else:
@@ -31,4 +36,9 @@ def changepass(request):
     pass
 
 def logout(request):
-    pass
+    if request.method == "POST":
+        if request.user.is_authenticated:
+            logout(request)
+            return redirect(request, 'welcome_view')
+    else:
+        redirect(request, 'welcome_view')
