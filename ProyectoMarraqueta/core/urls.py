7436b23b964290cb904aca53ce145bcce1a5ab31
Technopy311 <xmenmagnet@gmail.com>
2024-06-07 08:39:57 -0400

    chore(core.urls): Remove unused url

--- ProyectoMarraqueta/core/urls.py
+++ ProyectoMarraqueta/core/urls.py
@@ -3,7 +3,6 @@ from . import views as core_views
 
 urlpatterns = [
     path("user_view", core_views.user_view, name="user_view"),
-    path("user_register", core_views.user_register, name="user_register"),
     path("guard_view", core_views.guard_view, name="guard_view"),
     path("", core_views.welcome_view, name="welcome_view")
 ]
