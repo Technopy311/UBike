01fe4ce80e0a273a37a47dbc69a78200f97d94af
Technopy311 <xmenmagnet@gmail.com>
2024-06-07 22:18:52 -0400

    Attempt create custom_register function; Redirect guard user to guard_view when logging in

--- ProyectoMarraqueta/appauth/views.py
+++ ProyectoMarraqueta/appauth/views.py
@@ -20,15 +20,56 @@ def custom_login(request, next=None):
 
         if user is not None:
             login(request, user)
-            return redirect('user_view')
+            if user.is_guard:
+                return redirect('guard_view')
+            else:
+                return redirect('user_view')
         else:
             return redirect('welcome_view')
 
     else:
         return redirect('welcome_view')
 
-def register(request):
-    pass
+def custom_register(request, next=None):
+    return redirect('welcome_view')
+    if request.method == "POST":
+        nombre = request.POST["nombre"]
+        apellido = request.POST["apellido"]
+        rut = request.POST["rut"]
+        password = request.POST["password"]
+        email = request.POST["email"]
+        
+        modelBici = request.POST["modelBici"]
+        marcaBici = request.POST["marcaBici"]
+        colorBici = request.POST["colorBici"]
+        #fotoBici = request.FILES["fotoBici"]
+        print(request.FILES)
+
+        try:
+            usuario = core_models.User(
+                username = nombre,
+                last_name=apellido,
+                run=rut,
+                password=password,
+                email=email,
+            )
+            usuario.save()
+        except Exception:
+            pass
+
+        try:
+            bicicleta = core_models.Bicycle(
+                model=(str(marcaBici)+'-'+str(modelBici)),
+                colour=colorBici,
+                bicy_user=usuario,
+                image=fotoBici,
+            )
+            bicicleta.save()
+        except Exception:
+            pass
+
+    else:
+        return redirect('welcome_view')
 
 def changepass(request):
     pass
