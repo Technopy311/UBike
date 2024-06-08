ba985464a4b8611d8e3e7d11b68f6d0ac541153b
Technopy311 <xmenmagnet@gmail.com>
2024-06-07 21:16:31 -0400

    fix!(appauth.views): Fix dummy errors for custom login

--- ProyectoMarraqueta/appauth/views.py
+++ ProyectoMarraqueta/appauth/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from core import models as core_models
-
 from django.core.exceptions import PermissionDenied
 
-def login(request):
+def custom_login(request, next=None):
     if request.method == "POST":
         mail = request.POST["mail"]
         password = request.POST["password"]
@@ -12,22 +11,21 @@ def login(request):
         try:
             username = core_models.User.objects.get(email=mail).username
         except core_models.User.DoesNotExist:
-            return redirect(request, 'welcome_view') #TODO: add error message functionality for welcome_view
+            return redirect('welcome_view') #TODO: add error message functionality for welcome_view
         
         try:
-            user = authenticate(username=username, password=password)
+            user = authenticate(request, username=username, password=password)
         except PermissionDenied:
-            return redirect(request, 'welcome_view') #TODO: add error message functionality for welcome_view
+            return redirect('welcome_view') #TODO: add error message functionality for welcome_view
 
         if user is not None:
             login(request, user)
+            return redirect('user_view')
         else:
-            return redirect(request, 'welcome_view')
-
-        return redirect(request, 'user_view')
+            return redirect('welcome_view')
 
     else:
-        redirect(request, 'welcome_view')
+        return redirect('welcome_view')
 
 def register(request):
     pass
@@ -35,10 +33,10 @@ def register(request):
 def changepass(request):
     pass
 
-def logout(request):
+def logout(request, next):
     if request.method == "POST":
         if request.user.is_authenticated:
             logout(request)
-            return redirect(request, 'welcome_view')
+            return redirect('welcome_view')
     else:
-        redirect(request, 'welcome_view')
+        return redirect('welcome_view')
