7df44f3a24e8c23ce8464692fb6873aaded48d89
Technopy311 <xmenmagnet@gmail.com>
2024-06-07 21:17:14 -0400

    chore(appauth.urls):Change login function reference

--- ProyectoMarraqueta/appauth/urls.py
+++ ProyectoMarraqueta/appauth/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views as auth_views
 
 urlpatterns = [
-    path("login/", auth_views.login,name="login"),
+    path("login/", auth_views.custom_login,name="login"),
     path("register/", auth_views.register,name="register"),
     path("changepass/", auth_views.changepass,name="changepass"),
     path("logout/", auth_views.logout,name="logout"),
