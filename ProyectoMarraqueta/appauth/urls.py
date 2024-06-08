7501d09d1395ed8ec552e0678084ad15731401c8
Technopy311 <xmenmagnet@gmail.com>
2024-06-07 22:17:39 -0400

    chore!(appauth.urls): Change register view's associated function

--- ProyectoMarraqueta/appauth/urls.py
+++ ProyectoMarraqueta/appauth/urls.py
@@ -3,7 +3,7 @@ from . import views as auth_views
 
 urlpatterns = [
     path("login/", auth_views.custom_login,name="login"),
-    path("register/", auth_views.register,name="register"),
+    path("register/", auth_views.custom_register,name="register"),
     path("changepass/", auth_views.changepass,name="changepass"),
     path("logout/", auth_views.logout,name="logout"),
     
