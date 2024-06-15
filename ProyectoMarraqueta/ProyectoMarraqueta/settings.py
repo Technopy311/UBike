e106ee85f5789a5ed39cedeeb2d9f2d23de3ca47
Technopy311 <xmenmagnet@gmail.com>
2024-06-15 01:40:42 -0400

    fix!(ProyectoMarraqueta.settings): Change locale from es to en, for comma was beign used in floating values (dot needed)

--- ProyectoMarraqueta/ProyectoMarraqueta/settings.py
+++ ProyectoMarraqueta/ProyectoMarraqueta/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = "es"
+LANGUAGE_CODE = "en"
 
 TIME_ZONE = "America/Santiago"
 
