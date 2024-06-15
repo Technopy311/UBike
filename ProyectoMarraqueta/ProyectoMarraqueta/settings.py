bc65157e2426440b2616be66e29b3f66b092adfa
Technopy311 <xmenmagnet@gmail.com>
2024-06-15 00:16:47 -0400

    chore!(ProyectoMarraqueta.settings): Add media files support in settings

--- ProyectoMarraqueta/ProyectoMarraqueta/settings.py
+++ ProyectoMarraqueta/ProyectoMarraqueta/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,9 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [
+STATICFILES_DIRS = (
     BASE_DIR / "static",
-]
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -132,5 +133,7 @@ LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'user_view'
 LOGOUT_REDIRECT_URL = 'welcome_view'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 AUTH_USER_MODEL = "core.User"
--- ProyectoMarraqueta/ProyectoMarraqueta/urls.py
+++ ProyectoMarraqueta/ProyectoMarraqueta/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("", include("core.urls")),
     path("api/", include("picow_api.urls")),
     path("admin/", admin.site.urls),
     path("auth/", include("appauth.urls"))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
