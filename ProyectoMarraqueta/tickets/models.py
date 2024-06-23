cdd622edef94e1f81a5d7d556562a152898657da
Technopy311 <xmenmagnet@gmail.com>
2024-06-23 10:07:19 -0400

    chore(tickets.models): Create RegistryTicket and EmergencyTicket models

--- ProyectoMarraqueta/tickets/models.py
+++ ProyectoMarraqueta/tickets/models.py
@@ -1,3 +1,20 @@
+from typing import Any, Iterable
 from django.db import models
 
-# Create your models here.
+
+class RegistryTicket(models.Model):
+    user = models.ForeignKey("core.User", on_delete=models.CASCADE)
+    datetime = models.DateTimeField("Creation time", auto_now_add=True)
+    keychain = models.ForeignKey("core.KeyChain", on_delete=models.CASCADE)
+    
+    def save(self, *args, **kwargs):
+        if self.keychain.uuid != None:    
+            self.keychain.user = self.user
+        
+        super(RegistryTicket, self).save(*args, **kwargs)
+
+
+class EmergencyTicket(models.Model):
+    user = models.ForeignKey("core.User", on_delete=models.CASCADE)
+    datetime = models.DateTimeField("Creation time", auto_now_add=True)
+    message = models.CharField("Message", max_length=500, default=None)
