332a8cb63e196da05e0686b33041ecf19fa45ae3
Technopy311 <xmenmagnet@gmail.com>
2024-06-26 11:06:38 -0400

    feat(core.urls):add user_details url

--- ProyectoMarraqueta/core/urls.py
+++ ProyectoMarraqueta/core/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views as core_views
 
 urlpatterns = [
+    path("user_details/<user_pk>/", core_views.user_detail, name="user_detail"), 
     path("emergency_view", core_views.emergency_view, name="emergency_view"),
     path("user_view", core_views.user_view, name="user_view"),
     path("guard_view", core_views.guard_view, name="guard_view"),
